f660152b3dae5f1cf10c82d59278eb81e2db22eb
Raphael Faber <90429070+raphaelfaber@users.noreply.github.com>
2021-09-28 21:11:56 -0300

    Update calculadora_dos_gases_ideais.py

--- calculadora_dos_gases_ideais.py
+++ calculadora_dos_gases_ideais.py
@@ -1,12 +1,13 @@
 import time
+import os
 print('x'*60)
 print('Seja bem vindo(a) a calculadora da lei dos gases ideais!')
 print('x'*60)
-p=0
-v=0
-n=0
-r=0.082
-t=0
+p = 0
+v = 0
+n = 0
+r = 0.082
+t = 0
 while True:
 	var = str(input("""O que você deseja calcular?
 	Digite:
@@ -28,24 +29,26 @@ while True:
 			n = float(input('Digite o número de mols (n):  '))
 			t = float(input('Digite a temperatura(T) em kelvin:  '))
 			v = (n*r*t)/p
+			print('x' * 60)
 			print('O volume nas condições inseridaes é {} L'.format(v))
 		if var == 'n':
 			p = float(input('Digite a pressão (em ATM):  '))
 			v = float(input('Digite o volume (em L):  '))
 			t = float(input('Digite a temperatura(T) em kelvin:  '))
-			n=(p*v)/(r*t)
+			n = (p*v)/(r*t)
+			print('x' * 60)
 			print('A quantidade de mols nessas condições é de {} mol(s)'.format(n))
-		if var == 't': 
+		if var == 't':
 			p = float(input('Digite a pressão (em ATM):  '))
 			v = float(input('Digite o volume (em L):  '))
 			n = float(input('Digite o número de mols (n):  '))
-			t=(p*v)/(n*r)
+			t = (p*v)/(n*r)
+			print('x'*60)
 			print('A temperatura nessas condições é de {} K'.format(t))
 		loop = str(input(('Deseja calcular outro parâmetro?[Y/N]'))).lower().strip()
+		os.system('clear')
 		while loop not in ('ysn'):
 			loop = str(input(('Deseja calcular outro parâmetro?[Y/N]'))).lower().strip()
 		if loop == 'n':
 					print('Obrigado por usar a calculadora dos gases ideais!')
 					break
-          
-          
